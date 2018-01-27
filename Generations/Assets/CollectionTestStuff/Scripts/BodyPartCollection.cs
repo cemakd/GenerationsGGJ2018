1dd980cd172a2939282346c490c69228a1587e24
U-LAPTOP-KSJORU9T\Nicholas <nichgaun@umich.edu>
2018-01-26 23:03:45 -0500

    body part pickups adds them to simple UI and to body

---
+++ Generations/Assets/CollectionTestStuff/Scripts/BodyPartCollection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartCollection : MonoBehaviour {
+
+	public List<GameObject> body_collection;
+	public BodyPartDisplayer displayer;
+
+	// Use this for initialization
+	void Start () {
+		body_collection = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Add_Part(GameObject part) {
+		body_collection.Add (part);
+		displayer.Add_Part_Display (part);
+
+		part.transform.position = new Vector2 (transform.position.x + Random.Range(-1f, 1f), transform.position.y + Random.Range(0f, 1f));
+		part.transform.parent = transform;
+	}
+}
---
+++ Generations/Assets/CollectionTestStuff/Scripts/BodyPartDisplayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BodyPartDisplayer : MonoBehaviour {
+
+	public BodyPartCollection bpc;
+	public GameObject draw_on_UI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Add_Part_Display(GameObject part) {
+		//create child UI image
+		GameObject display = Instantiate(draw_on_UI, transform);
+
+		//set display's position based on number already collected
+		int num_collected = bpc.body_collection.Count;
+		display.transform.position = new Vector2 (display.transform.position.x + (num_collected-1) * 50, display.transform.position.y);
+
+		//set image component image to collected
+		display.GetComponent<Image> ().sprite = part.GetComponent<SpriteRenderer>().sprite;
+	}
+}
