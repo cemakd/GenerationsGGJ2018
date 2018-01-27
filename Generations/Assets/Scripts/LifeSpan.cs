2b396b8ee8c7845a7be191351e3accf90f62e98b
Eray Mitrani <eraymitrani1@hotmail.com>
2018-01-27 15:45:45 -0500

    added lifespan visual

---
+++ Generations/Assets/Scripts/LifeSpan.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeSpan : MonoBehaviour
+{
+
+	private SpriteRenderer sr;
+	private Color currentColor;
+	[SerializeField] public float timeToDeath;
+	private float t ;
+	// Use this for initialization
+	void Start ()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		currentColor = sr.color;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Color updateColor = Color.Lerp(currentColor, Color.black, t);
+		updateColor.a = Mathf.Lerp(1, 0.7f, t);
+		sr.color = updateColor;
+		if (t < 1)
+		{
+			t += Time.deltaTime / timeToDeath;
+		}
+		
+	}
+}
