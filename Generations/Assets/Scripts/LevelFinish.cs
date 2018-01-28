b476f8a1ee7c5a6621af1129c82829efb140068d
U-LAPTOP-KSJORU9T\Nicholas <nichgaun@umich.edu>
2018-01-28 14:14:26 -0500

    mate text

--- Generations/Assets/Scripts/LevelFinish.cs
+++ Generations/Assets/Scripts/LevelFinish.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets._2D;
+using UnityEngine.UI;
 
 public class LevelFinish : MonoBehaviour {
 
     private SceneLoader sl;
+	public GameObject mate_instruction;
+	private GameObject mate_text;
 
     void Start() {
+		mate_text = Instantiate (mate_instruction, new Vector2 (1000, 100), Quaternion.identity);
+		mate_text.SetActive (false);
+
         GameObject slObj = GameObject.Find("SceneLoader");
         if (slObj != null)
             sl = slObj.GetComponent<SceneLoader>();
@@ -16,6 +22,8 @@ public class LevelFinish : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
         GameObject otherGO = other.gameObject;
         if (sl != null && otherGO.GetComponent<PlatformerCharacter2D>() != null) {
+			mate_text.SetActive (true);
+
             GameObject matingPanel = GameObject.Find("Mating Ritual");
             MatingRitualManager mrm = matingPanel.GetComponent<MatingRitualManager>();
             mrm.mateUpgrades = GetComponent<PlayerUpgrades>();
@@ -25,6 +33,7 @@ public class LevelFinish : MonoBehaviour {
     void OnTriggerExit2D(Collider2D other) {
         GameObject otherGO = other.gameObject;
         if (sl != null && otherGO.GetComponent<PlatformerCharacter2D>() != null) {
+			mate_text.SetActive (false);
             sl.stageCanEnd = false;
         }
     }
