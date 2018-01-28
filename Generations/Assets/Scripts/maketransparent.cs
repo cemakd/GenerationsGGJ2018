50153e09effe29587ef0d017d1f34b887bc01101
Eray Mitrani <eraymitrani1@hotmail.com>
2018-01-27 22:44:58 -0500

    scene

---
+++ Generations/Assets/Scripts/maketransparent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maketransparent : MonoBehaviour
+{
+
+	private SpriteRenderer sr;
+	// Use this for initialization
+	void Start ()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0.7f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
