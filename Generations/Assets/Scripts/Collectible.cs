94216d98b2ab1636a141c6887d646c9c057bad54
Cem Akdemir <cem.akd@hotmail.com>
2018-01-28 00:46:38 -0500

    New icons

--- Generations/Assets/Scripts/Collectible.cs
+++ Generations/Assets/Scripts/Collectible.cs
@@ -12,7 +12,31 @@ public class Collectible : MonoBehaviour {
         Vision
     }
 
+    public Sprite strImg;
+    public Sprite agiImg;
+    public Sprite fliImg;
+    public Sprite visImg;
+
     public CollectibleType type;
+
+    void Start() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.color = new Color(255, 255, 255, 1);
+        switch (type) {
+            case CollectibleType.Strenght:
+                sr.sprite = strImg;
+                break;
+            case CollectibleType.Agility:
+                sr.sprite = agiImg;
+                break;
+            case CollectibleType.Flight:
+                sr.sprite = fliImg;
+                break;
+            case CollectibleType.Vision:
+                sr.sprite = visImg;
+                break;
+        }
+    }
 
 	void OnTriggerEnter2D (Collider2D other) {
 		AttributeCollection bpc = other.GetComponent<AttributeCollection> ();
