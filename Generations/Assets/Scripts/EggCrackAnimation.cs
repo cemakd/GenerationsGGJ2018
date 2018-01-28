1b03c583407372c504df14b688a0d8e00f083d7e
Eray Mitrani <eraymitrani1@hotmail.com>
2018-01-28 14:32:19 -0500

    egg animation

---
+++ Generations/Assets/Scripts/EggCrackAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggCrackAnimation : MonoBehaviour {
+
+	public Sprite[] eggSprites;
+	private SpriteRenderer sr;
+	public GameObject p;
+	// Use this for initialization
+	void Start()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		StartCoroutine("CrackAnimation");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public IEnumerator CrackAnimation()
+	{
+		yield return new WaitForSeconds(0.4f);
+		sr.sprite = eggSprites[0];
+		yield return new WaitForSeconds(0.4f);
+		sr.sprite = eggSprites[1];
+		yield return new WaitForSeconds(0.4f);
+		sr.sprite = eggSprites[2];
+		yield return new WaitForSeconds(0.4f);
+		Destroy(this.gameObject);
+		Instantiate(p, transform.position, Quaternion.identity);
+		yield return null;
+	}
+}
